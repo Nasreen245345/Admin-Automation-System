20794fd27ca76d13c85d8e8271566d69dd3105c2
Zahidmajeed804 <zahidmajeed804@gmail.com>
2026-09-17 11:41:32 +0500

    AAS-86 Attendance Mongoose model

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,3 +3,4 @@ export { default as Role } from "./Role.js";
 export { default as Permission } from "./Permission.js";
 export { default as UserRole } from "./UserRole.js";
 export { default as RolePermission } from "./RolePermission.js";
+export { default as Attendance } from "./Attendance.js";
